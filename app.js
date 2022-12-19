8b0e6b01fbee8843ea08870686b6375cb7634381
frank <frankcool321@gmail.com>
2022-12-19 21:50:58 +0800

    refactor: add home page link and  modify ui

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 //- require related modules
 const express = require("express");
 const exphbs = require("express-handlebars");
-
-const router = require("./routes/index")
+const router = require("./routes/index");
 //- connect to db
 require("./config/mongoose");
 
@@ -18,9 +17,9 @@ app.use(express.static("public"));
 //- bodyparser
 app.use(express.urlencoded({ extended: true }));
 //- use router
-app.use(router)
+app.use(router);
 
 //- listen to server
 app.listen(port, () => {
-  console.log("Server is listening to http://localhost:3000");
+  console.log("Server is listening to http://localhost:3000/home");
 });
